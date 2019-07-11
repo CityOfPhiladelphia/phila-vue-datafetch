511a970d48587b7b613c5765462a08d772c1e526
stevetotheizz0 <stephenjskilton@gmail.com>
2019-07-11 10:34:55 -0400

    Update shape-search-client.js

--- src/controller/clients/shape-search-client.js
+++ src/controller/clients/shape-search-client.js
@@ -6,7 +6,7 @@ import BaseClient from './base-client';
 class ShapeSearchClient extends BaseClient {
 
   evaluateParams(feature, dataSource) {
-    console.log('shape-search-client evaluateParams is running');
+    //console.log('shape-search-client evaluateParams is running');
     const params = {};
     if (!dataSource.options.params) { return params };
     const paramEntries = Object.entries(dataSource.options.params);
@@ -27,7 +27,7 @@ class ShapeSearchClient extends BaseClient {
   }
 
   evaluateDataForUnits(data) {
-    console.log('shape-search-client evaluateDataForUnits data: ', data);
+    //console.log('shape-search-client evaluateDataForUnits data: ', data);
     // console.log("evaluateDataForUnits dataRows: ",dataRows);
     let groupedData = _.groupBy(data.rows, a => a.pwd_parcel_id);
     // console.log("evaluateDataForUnits groupedData: ", groupedData);
@@ -64,7 +64,7 @@ class ShapeSearchClient extends BaseClient {
   }
 
   fetch(input) {
-    console.log('shape-search-client fetch is running, input:', input);
+    //console.log('shape-search-client fetch is running, input:', input);
     const data =  input.map(a => a.properties.PARCELID)
     // console.log('shapeSearch DATA', data);
 
