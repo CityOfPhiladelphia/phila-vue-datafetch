727b3b0f1871cbecf4a20e483fd3315c70118c92
andy.rothwell <rothwell.andy@gmail.com>
2019-10-04 14:44:32 -0400

    testing breaking a test deliberately with travis

--- test/controller.test.js
+++ test/controller.test.js
@@ -42,7 +42,7 @@ describe('handleSearchFormSubmit', () => {
     expect(store.state.sources.opa.data.depth).toEqual('64');
     expect(store.state.sources.opa.data.zoning).toEqual('RSA5 ');
   });
-  
+
   test('Commercial Parcel, regular', async () => {
     await Promise.all([
       controller.handleSearchFormSubmit('1234 mkt'),
@@ -53,31 +53,32 @@ describe('handleSearchFormSubmit', () => {
     expect(store.state.sources.opa.data.depth).toEqual('190');
     expect(store.state.sources.opa.data.zoning).toEqual('CMX5 ');
   });
-  
+
     test('DoR No PWD Option 1', async () => {
     await Promise.all([
         controller.handleSearchFormSubmit('6117 nassau'),
         timeout(3000)
       ]);
-    
+
     expect(store.state.geocode.data.properties.street_address).toEqual('6117 NASSAU RD');
     expect(store.state.parcels.dor.activeMapreg).toEqual("068N220217");
     expect(store.state.parcels.pwd.properties.BRT_ID).toEqual("342085700");
     expect(store.state.sources.opa.data.depth).toEqual("93.25");
   });
-  
+
   test('DoR No PWD Option 2', async () => {
     await Promise.all([
         controller.handleSearchFormSubmit('5945 lawndale'),
         timeout(3000)
       ]);
-    
+
     expect(store.state.geocode.data.properties.street_address).toEqual('5945 LAWNDALE ST');
-    expect(store.state.parcels.dor.activeMapreg).toEqual("137N230256");
+    expect(store.state.parcels.dor.activeMapreg).toEqual("jjj");
+    // expect(store.state.parcels.dor.activeMapreg).toEqual("137N230256");
     expect(store.state.parcels.pwd.properties.BRT_ID).toEqual("352322900");
     expect(store.state.sources.opa.data.depth).toEqual("65");
   });
-  
+
   test('PWD No DoR Parcel Option 1', async () => {
     await Promise.all([
       controller.handleSearchFormSubmit('5208 Wayne Ave'),
@@ -89,7 +90,7 @@ describe('handleSearchFormSubmit', () => {
     expect(store.state.sources.opa.data.depth).toEqual('198.5');
     expect(store.state.sources.opa.data.zoning).toEqual('RTA1 ');
   });
-  
+
   test('PWD No DoR Parcel Option 2', async () => {
     await Promise.all([
       controller.handleSearchFormSubmit('3674 Richmond'),
@@ -121,7 +122,7 @@ describe('handleMapClick', () => {
       // check sources
       expect(store.state.sources.opa.data.parcel_number).toEqual('301286300');
     });
-    
+
     test('Click on 3100 Penrose Ferry Rd', async () => {
       store.commit('setActiveParcelLayer', 'pwd');
       store.commit('setLastSearchMethod', 'reverseGeocode');
@@ -129,7 +130,7 @@ describe('handleMapClick', () => {
         controller.handleMapClick({ originalEvent: { keycode: 0 },  latlng: { lng: -75.1878948516403, lat: 39.913635291857084 }}),
         timeout(3000) // Add a timeout to allow fetchData to complete
       ]);
-      
+
       // check geocode
       expect(store.state.geocode.data.properties.street_address).toEqual('3100 PENROSE FERRY RD');
       // check parcels
@@ -140,7 +141,7 @@ describe('handleMapClick', () => {
       expect(store.state.sources.liPermits.data.rows[0]._featureId).toEqual('feat-liPermits-0');
     });
   });
-  
+
   describe('dor activeParcel', () => {
     test('Click on random house', async () => {
       store.commit('setActiveParcelLayer', 'dor');
@@ -158,7 +159,7 @@ describe('handleMapClick', () => {
       // check sources
       expect(store.state.sources.opa.data.parcel_number).toEqual('301286300');
     });
-    
+
     test('Click on dor parcel 2306 HARTRANFT ST # 102', async () => {
       store.commit('setActiveParcelLayer', 'dor');
       store.commit('setLastSearchMethod', 'reverseGeocode');
