429ca8f06b8c2d7c9752c97ea2d38de3fd0baba7
stevetotheizz0 <stephenjskilton@gmail.com>
2020-07-01 12:22:39 -0400

    feature: started pagination for over 100 results in block search

--- src/clients/block-search-client.js
+++ src/clients/block-search-client.js
@@ -16,7 +16,6 @@ class BlockSearchClient extends BaseClient {
 
     // update state
     this.store.commit('setBlockSearchStatus', 'waiting');
-    this.store.commit('setOwnerSearchStatus', 'waiting');
     // console.log('block SEARCH CLIENT setting last search method to block search');
     this.store.commit('setLastSearchMethod', 'block search');
 
@@ -35,14 +34,75 @@ class BlockSearchClient extends BaseClient {
     const store = this.store;
     const data = response.data;
     const url = response.config.url;
+    // let features = data.features;
+    let params = response.config.params;
+    
 
     if (!data.features || data.features.length < 1) {
       // console.log('block search got no features', data);
-
       return;
     }
 
+    async function getPages(features) {
+      console.log('getPages is running still going 2, pages:', this, features);
+      console.log(blockSearchConfig);
+
+      let pages = Math.ceil(data.total_size / 100);
+
+      if (pages > 1) {
+        for (let counter = 2; counter<=pages; counter++) {
+          params.page = counter;
+          console.log('in loop, counter:', counter, this, params.page);
+          let pageResponse = await axios.get(url, { params });
+          console.log("page response: ", pageResponse);
+          features = await features.concat(pageResponse.data.features);
+          console.log('response:', pageResponse, 'features:', features);
+        }
+      }
+
+      console.log(features);
+
+      // let units = features.filter(a => a.properties.unit_num != "");
+      // units = this.evaluateDataForUnits(units);
+
+      // var feature = JSON.parse(JSON.stringify(units[0]));
+      // for (let i in feature.properties) {
+      //   feature.properties[i] = "";
+      // }
+
+      // if(this.store.state.parcels.pwd === null) {
+      //   // this.setFeatureProperties(feature, totalUnits);
+
+      //   console.log('condo-search-client, getPages else is still running 1');
+      //   store.commit('setGeocodeData', feature);
+      //   store.commit('setGeocodeStatus', 'success');
+      //   // console.log('getPages else is still running 2');
+      //   if (this.store.state.lastSearchMethod !== 'reverseGeocode') {
+      //     this.store.commit('setLastSearchMethod', 'geocode');
+      //   }
+      //   // console.log('feature:', feature);
+      // } else {
+      //   this.setFeatureProperties(feature, totalUnits);
+
+      //   console.log('condo-search-client getPages else is still running 1');
+      //   store.commit('setGeocodeData', feature);
+      //   store.commit('setGeocodeStatus', 'success');
+      // console.log('getPages else is still running 2');
+      // console.log('feature:', feature);
+      // }
+
+      // this.store.commit('setCondoUnitsStatus', 'success');
+      // return feature;
+      // }
+    }
+    
     let features = data.features;
+
+    let pages = Math.ceil(data.total_size / 100);
+    console.log("pages: ", pages);
+    features =  (pages > 1 ? features = getPages.bind(this) : features);
+
+    console.log("block features: ", features);
     features = this.assignFeatureIds(features, 'block');
 
  
